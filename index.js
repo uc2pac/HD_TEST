4d61533ffb3c4bdfd487bf4c81dbbf2c803f7347
Yaroslav Basovskyy <ybasovskyy@ybasovskyy-it-us-macbook-pro.local>
2018-03-21 16:45:43 -0400

    change request to async for unload event

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ var lzString = require('./utils/lzString');
 var app = express();
 var port = 8080;
 
+var tempStorage = [];
+
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.text());
 
+function pushToStorage(data) {
+  console.dir(JSON.parse(data), {depth: null, colors: true});
+}
+
 // serve static pages
 app.post('/analytics', function(req, res){
   var decoded = decodeURIComponent(req.body);
   var decompressed = lzString.decompressFromBase64(decoded);
+
+  pushToStorage(decompressed);
+
   res.status(200).send(decompressed);
 });
 
