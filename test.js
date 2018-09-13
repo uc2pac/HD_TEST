4b34e0958e485f2f585d2d552646d1e06741e0f2
Yaroslav Basovskyy <yaroslav_basovskyy@homedepot.com>
2018-09-13 14:56:50 -0400

    test4

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
 const version = require('./package.json').version;
 
 console.log(version);
+
+var fs = require('fs');
+fs.writeFile("./dist/test.txt", `Hey there! Version: ${version}`, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 
